6c776f5a822b5805eee69bfe7dd43185fa3e5596
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-14 21:13:24 +0800

    feat: add service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,68 @@
-// const CACHE_NAME = 'pomodoro-app-v1';
-// const urlsToCache = [
-//   '/',
-//   '/index.html',
-//   '/css/styles.css',
-//   '/js/app.js',
-//   '/js/pomodoro.js',
-//   '/audio/focus-start.mp3',
-//   '/audio/break-start.mp3',
-//   '/images/favicon.ico',
-//   '/images/logo.png'
-// ];
+const CACHE_NAME = 'pomodoro-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
 
-// self.addEventListener('install', event => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(cache => cache.addAll(urlsToCache))
-//   );
-// });
+  '/css/button.css',
+  '/css/modals.css',
+  '/css/preflight.css',
+  '/css/styles.css',
+  '/css/typography.css',
+  '/css/variables.css',
 
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(response => {
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request);
-//       })
-//   );
-// });
+  '/js/app.js',
+  '/js/Audio.js',
+  '/js/ControlButtons.js',
+  '/js/DoneModal.js',
+  '/js/Images.js',
+  '/js/Pomodoro.js',
+  '/js/Timer.js',
+  '/js/TimerWorker.js',
 
-// self.addEventListener('activate', event => {
-//   const cacheWhitelist = [CACHE_NAME];
-//   event.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.map(cacheName => {
-//           if (cacheWhitelist.indexOf(cacheName) === -1) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
+  '/audio/pomodoro-alert.mp3',
+
+  '/fonts/AvantGardeGothic/Regular.otf',
+  '/fonts/AvantGardeGothic/Medium.otf',
+  '/fonts/AvantGardeGothic/Bold.otf',
+
+  '/images/app-icons/focus-mode.png',
+  '/images/app-icons/reset-logged-time.svg',
+  '/images/app-icons/taking-a-break.png',
+  '/images/android-chrome-192x192.png',
+  '/images/android-chrome-512x512.png',
+  '/images/apple-touch-icon.png',
+  '/images/favicon.ico',
+  '/images/favicon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
